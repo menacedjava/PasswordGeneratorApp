062788c25ec66455f20c28128165873a4cebd2de
Madumarov Davron <menaceteacher@gmail.com>
2025-01-26 03:22:21 +0500

    add

---
+++ src/main/java/org/example/PasswordGeneretor.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+public class PasswordGeneretor {
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+    private static final String ALL_CHARACTERS = UPPERCASE + LOWERCASE + DIGITS + SPECIAL_CHARACTERS;
+
+    public static void main(String[] args) {
+
+    }
+}
