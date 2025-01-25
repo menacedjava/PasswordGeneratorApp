0580091b6678ca1822bcb304c3d5e97db2f0b70a
Madumarov Davron <menaceteacher@gmail.com>
2025-01-26 03:22:45 +0500

    add

--- src/main/java/org/example/PasswordGeneretor.java
+++ src/main/java/org/example/PasswordGeneretor.java
@@ -1,13 +1,29 @@
 package org.example;
 
 
+import java.security.SecureRandom;
+
 public class PasswordGeneretor {
     private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
     private static final String DIGITS = "0123456789";
     private static final String SPECIAL_CHARACTERS = "!@#$%^&*()-_=+[]{}|;:,.<>?";
     private static final String ALL_CHARACTERS = UPPERCASE + LOWERCASE + DIGITS + SPECIAL_CHARACTERS;
+    public static String generatePassword(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
 
+        password.append(UPPERCASE.charAt(random.nextInt(UPPERCASE.length())));
+        password.append(LOWERCASE.charAt(random.nextInt(LOWERCASE.length())));
+        password.append(DIGITS.charAt(random.nextInt(DIGITS.length())));
+        password.append(SPECIAL_CHARACTERS.charAt(random.nextInt(SPECIAL_CHARACTERS.length())));
+
+        for (int i = 4; i < length; i++) {
+            password.append(ALL_CHARACTERS.charAt(random.nextInt(ALL_CHARACTERS.length())));
+        }
+
+        return shuffleString(password.toString());
+    }
     public static void main(String[] args) {
 
     }
